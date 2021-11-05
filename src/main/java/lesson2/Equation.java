8c8c737d61c38205db6526d1c1feb0f1f7ecfd34
Blonde-O <blonde-o@yandex.ru>
2021-11-05 23:25:58 +0300

    Второе домашнее задание. Формула силы трения на наклонной поверхности. С вводом и выводом в консоль.

--- src/main/java/lesson2/Equation.java
+++ src/main/java/lesson2/Equation.java
@@ -24,8 +24,8 @@ public class Equation {
 
     public static double frictionForce(double mas, double frict, double ugol) {
         double ves = 9.81 * mas;
-        double sinus = Math.sin( Math.toRadians(ugol));
-        double friction = ves * frict * sinus;
+        double cosinus = Math.cos( Math.toRadians(ugol));
+        double friction = ves * frict * cosinus;
                return friction;
     }
     }
