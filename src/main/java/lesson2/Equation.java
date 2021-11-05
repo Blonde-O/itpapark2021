b91f648d9445f0bb728d4860377f5b5b74e8f426
Blonde-O <blonde-o@yandex.ru>
2021-11-05 23:06:32 +0300

    Второе домашнее задание. Формула силы трения на наклонной поверхности. С вводом и выводом в консоль.

---
+++ src/main/java/lesson2/Equation.java
@@ -0,0 +1,31 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Equation {
+
+    public static void main(String[] args) {
+
+        Scanner in  = new Scanner(System.in);
+
+        System.out.println("Данная программа вычисляет силу трения на наклонной поверхности.");
+        System.out.println("Введите массу объекта в кг: ");
+        double mas = in.nextDouble();
+        System.out.println("Введите коэффициент трения: ");
+        double frictionCoefficient = in.nextDouble();
+        System.out.println("Введите угол наклона пов-ти в градусах: ");
+        double incline = in.nextDouble();
+        System.out.print("Сила трения составляет: ");
+        double result = frictionForce( mas, frictionCoefficient, incline);
+        System.out.printf("%.2f", result);
+        System.out.print(" H");
+        in.close();
+    }
+
+    public static double frictionForce(double mas, double frict, double ugol) {
+        double ves = 9.81 * mas;
+        double y = Math.sin( Math.toRadians(ugol));
+        double friction = ves * frict * y;
+               return friction;
+    }
+    }
