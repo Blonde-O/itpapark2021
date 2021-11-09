aef20ca3c218b9d40a6e615228ac28174941f583
Blonde-O <blonde-o@yandex.ru>
2021-11-09 12:42:14 +0300

    Решатель квадратного уравнения заданию №3

--- src/main/java/lesson3/QuadraticEquation.java
+++ src/main/java/lesson3/QuadraticEquation.java
@@ -1,6 +1,6 @@
 package lesson3;
 
-public class QuadraticEquation<d> {
+public class QuadraticEquation {
 
     public static void main(String[] args) {
         double a = 2;
@@ -11,55 +11,46 @@ public class QuadraticEquation<d> {
         if (a == 0) {
             System.out.println("Первый коэффициент не может быть 0");
             return;
-        } else {
         }
-
         if (d == 0) {
-            double z = solution1(a, b, c);
-            String result = String.format("%.2f", z);
-            System.out.print("Единственное решение уравнения  ");
-            System.out.println(result);
-        } else {
+            System.out.println("Дискриминант равен  " +d);
+            double z = solution1(a, b);
+            String result = String.format("%.3f", z);
+            System.out.println("Единственное решение уравнения  " + result);
         }
         if (d < 0) {
+            System.out.println("Дискриминант равен  " +d);
             System.out.println("Нет действительных решений уравнения");
-        } else {
         }
         if (d > 0) {
+            System.out.println("Дискриминант равен  " +d);
             double z1 = solution2(a, b, c);
-            String result1 = String.format("%.2f", z1);
+            String result1 = String.format("%.3f", z1);
             double z2 = solution3(a, b, c);
-            String result2 = String.format("%.2f", z2);
-            System.out.print("Первое решение уравнения:   ");
-            System.out.println(result1);
-            System.out.print("Второе решение уравнения:   ");
-            System.out.println(result2);
-        } else {
+            String result2 = String.format("%.3f", z2);
+            System.out.println("Первое решение уравнения:   " + result1);
+            System.out.println("Второе решение уравнения:   " + result2);
         }
-        return;
     }
 
     public static double discriminant(double a, double b, double c) {
-        double d = Math.pow(b, 2) - 4 * a * c;
-        return d;
+        return Math.pow(b, 2) - 4 * a * c;
     }
 
-    public static double solution1(double a, double b, double c) {
-        double x = -b / 2 * a;
-        return x;
+    public static double solution1(double a, double b) {
+        return -b / 2 * a;
     }
 
     public static double solution2(double a, double b, double c) {
         double d = Math.pow(b, 2) - 4 * a * c;
-        double x1 = (-b + Math.sqrt(d)) / 2 * a;
-        return x1;
+        return (-b + Math.sqrt(d)) / 2 * a;
+
 
     }
 
     public static double solution3(double a, double b, double c) {
         double d = Math.pow(b, 2) - 4 * a * c;
-        double x2 = (-b - Math.sqrt(d)) / 2 * a;
-        return x2;
+        return (-b - Math.sqrt(d)) / 2 * a;
 
     }
 }
