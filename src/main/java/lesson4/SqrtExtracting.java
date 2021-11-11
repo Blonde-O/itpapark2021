13da133eb7838c6566133d0105985bdf11c2ca5c
Blonde-O <blonde-o@yandex.ru>
2021-11-11 16:51:14 +0300

    Извлечение корня

--- src/main/java/lesson4/SqrtExtracting.java
+++ src/main/java/lesson4/SqrtExtracting.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 
 public class SqrtExtracting {
 
-         public static void main(String[] args) {
-         Scanner in = new Scanner(System.in);
-         System.out.println("Введите число, из которого извлекаете квадратный корень");
-         double n = in.nextDouble();
-         if (n<0){System.out.println("Недопустимое значение");
-         return;}
-         if (n==0){System.out.println("Результат = 0");}
-         else{
-         System.out.println("Результат = " +searchForArgument(n));
-         System.out.println("Проверка результата  " +searchForArgument(n)*searchForArgument(n));
-         }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число, из которого извлекаете квадратный корень");
+        double n = in.nextDouble();
+        if (n < 0) {
+            System.out.println("Недопустимое значение");
+            return;
         }
-
-        public static double searchForArgument(double n) {
-            double a = 0;
-            do {
-               a= a + 0.00001;            }
-            while (n-(a*a)>0);
-            return a;
+        if (n == 0) {
+            System.out.println("Результат = 0");
+        } else {
+            System.out.println("Результат = " + searchForArgument(n));
+            System.out.println("Проверка результата  " + searchForArgument(n) * searchForArgument(n));
         }
     }
+
+    public static double searchForArgument(double n) {
+        double a = 0;
+        do {
+            a = a + 0.00001;
+        }
+        while (n - (a * a) > 0);
+        return a;
+    }
+}
