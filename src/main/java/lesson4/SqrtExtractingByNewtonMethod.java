9fae2c9b640b20e8e0af4fbcf642daf5b38b7c4c
Blonde-O <blonde-o@yandex.ru>
2021-11-12 17:21:19 +0300

    Извлечение корня методом Ньютона

--- src/main/java/lesson4/SqrtExtractingByNewtonMethod.java
+++ src/main/java/lesson4/SqrtExtractingByNewtonMethod.java
@@ -21,8 +21,8 @@ public class SqrtExtractingByNewtonMethod {
 
     public static double sqrt(double n) {
         double a = 1;
-        for (double i = 1; i <= 10000; i++) {
-            a = a - (a * a - n) / (2 * n);
+        for (double i = 1; i <= 10; i++) {
+            a = a - (a * a - n) / (2 * a);
         }
         return a;
 
