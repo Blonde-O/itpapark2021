f74810c2c5b867a632caacf848368d222daa2f4e
Blonde-O <blonde-o@yandex.ru>
2021-11-12 16:43:08 +0300

    Извлечение корня методом Ньютона

--- src/main/java/lesson4/SqrtExtractingByNewtonMethod.java
+++ src/main/java/lesson4/SqrtExtractingByNewtonMethod.java
@@ -7,13 +7,21 @@ public class SqrtExtractingByNewtonMethod {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число, из которого хотите извлечь квадратный корень");
         double n = in.nextDouble();
-        System.out.println("Ваш результат равен  " + sqrt(n));
-        System.out.println("Проверка результата  " + sqrt(n) * sqrt(n));
+        if (n < 0) {
+            System.out.println("Некорректный ввод  ");
+            return;
+        }
+        if (n == 0) {
+            System.out.println("Ваш результат равен  0");
+        } else {
+            System.out.println("Ваш результат равен  " + sqrt(n));
+            System.out.println("Проверка результата  " + sqrt(n) * sqrt(n));
+        }
     }
 
     public static double sqrt(double n) {
         double a = 1;
-        for (double i = 1; i <= 1000; i++) {
+        for (double i = 1; i <= 10000; i++) {
             a = a - (a * a - n) / (2 * n);
         }
         return a;
