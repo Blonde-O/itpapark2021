e9ab5f7d31f5e19a86a3323aa6ef572c644b7c64
Blonde-O <blonde-o@yandex.ru>
2021-11-19 18:49:20 +0300

    Действия с массивами

--- src/main/java/lesson6/ActionsWithArray.java
+++ src/main/java/lesson6/ActionsWithArray.java
@@ -16,18 +16,18 @@ public class ActionsWithArray {
             array[i] = a;
         }
         System.out.println("Ваш массив: \n" + Arrays.toString(array));
-        System.out.println("\nСреднее арифметическое Вашего массива равно:  " + average(array, n));
+        System.out.println("\nСреднее арифметическое Вашего массива равно:  " + average(array));
         System.out.println("Минимальное значение Вашего массива равно:  " + minimalValue(array));
         System.out.println("Максимальное значение Вашего массива равно:  " + maximalValue(array));
         in.close();
     }
 
-    public static double average(int[] array, int n) {
+    public static double average(int[] array) {
         double average = 0;
         for (int b : array) {
             average += b;
         }
-        return average / n;
+        return average / array.length;
     }
 
     public static int minimalValue(int[] array) {
