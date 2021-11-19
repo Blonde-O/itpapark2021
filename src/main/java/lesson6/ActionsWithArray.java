c704ac049b7acef9c44a6e43eedba73b6bfa3f57
Blonde-O <blonde-o@yandex.ru>
2021-11-19 15:23:39 +0300

    Методы мин, макс, средн для массива

--- src/main/java/lesson6/ActionsWithArray.java
+++ src/main/java/lesson6/ActionsWithArray.java
@@ -12,20 +12,41 @@ public class ActionsWithArray {
         int n = in.nextInt();
         int[] array = new int[n];
         for (int i = 0; i < array.length; i++) {
-            int a = (int) Math.round( Math.random() * 10f);
+            int a = (int) Math.round(Math.random() * 100);
             array[i] = a;
         }
         System.out.println("Ваш массив: \n" + Arrays.toString(array));
-        System.out.println("Среднее арифметическое Вашего массива равно:  " + average(array, n));
+        System.out.println("\nСреднее арифметическое Вашего массива равно:  " + average(array, n));
+        System.out.println("Минимальное значение Вашего массива равно:  " + minimalValue(array));
+        System.out.println("Максимальное значение Вашего массива равно:  " + maximalValue(array));
         in.close();
     }
 
     public static double average(int[] array, int n) {
         double average = 0;
-        for (double b : array) {
+        for (int b : array) {
             average += b;
         }
         return average / n;
     }
 
+    public static int minimalValue(int[] array) {
+        int b = array[0];
+        for (int i = 1; i < array.length; ++i) {
+            if (b >= array[i]) {
+                b = array[i];
+            }
+        }
+        return b;
+    }
+
+    public static int maximalValue(int[] array) {
+        int b = array[0];
+        for (int i = 1; i < array.length; ++i) {
+            if (b <= array[i]) {
+                b = array[i];
+            }
+        }
+        return b;
+    }
 }
