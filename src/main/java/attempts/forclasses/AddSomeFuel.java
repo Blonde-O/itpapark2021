6c5596716301c856229e19807f34e88147cf7ea5
Blonde-O <blonde-o@yandex.ru>
2021-11-25 17:31:25 +0300

    Классы машин

--- src/main/java/attempts/forclasses/AddSomeFuel.java
+++ src/main/java/attempts/forclasses/AddSomeFuel.java
@@ -6,16 +6,21 @@ public class AddSomeFuel {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите категорию ТС (A,B или C)");
+        System.out.println("Введите категорию ТС (мотоцикл ,легковая  или грузовая)");
         String cat = in.next();
         System.out.println("Введите расстояние");
         int dist = in.nextInt();
+        System.out.println("Введите заполненность бака в %");
+        int fullness = in.nextInt();
 
-        Vehicle car = new Vehicle(cat, dist);
+        Vehicle car = new Vehicle(dist);
+        car.setCategory(cat);
+        car.setTankFullness(fullness);
         car.determinationOfVehicle();
-        System.out.println("Для категории " + cat+  " объем бака составляет " +car.getTankCapacity()+ "л., а расход топлива " +car.getConsumption()+ "л/100км.");
-
-
+        car.determinationOfFuel();
+        System.out.println("Для категории " + car.getCategory()+  " объем бака составляет " +car.getTankCapacity()+ "л., а расход топлива " +car.getConsumption()+ "л/100км.");
+        System.out.println("Чтобы проехать " +dist+ " километров, вам нужно " +car.getReserve()+ " литров топлива");
+        System.out.println("Вам необходимо заправиться " + car.getRefueling()+ " раз.");
 
 
     }
--- src/main/java/attempts/forclasses/Vehicle.java
+++ src/main/java/attempts/forclasses/Vehicle.java
@@ -5,33 +5,63 @@ public class Vehicle {
     int tankCapacity;
     int consumption;
     int distance;
+    double reserve;
+    int tankFullness;
+    int refueling;
+
+    Vehicle(int distance) {
+        //this.category = category;
+        this.distance = distance;
+    }
+
+    public void setTankFullness(int tankFullness) {
+        this.tankFullness = tankFullness;
+    }
+
+    public int getRefueling() {
+        return refueling;
+    }
 
     public int getTankCapacity() {
         return tankCapacity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getReserve() {
+        return reserve;
     }
 
     public int getConsumption() {
         return consumption;
     }
 
-    public Vehicle(String category, int distance) {
-        this.category = category;
-        this.distance = distance;
-    }
-    public void determinationOfVehicle(){
-        if ("A".equals(this.category)){
-           tankCapacity = 15;
-           consumption = 3;
+    void determinationOfVehicle() {
+        if ("мотоцикл".equals(this.category)) {
+            tankCapacity = 15;
+            consumption = 3;
         }
-        if ("B".equals(this.category)){
+        if ("легковая".equals(this.category)) {
             tankCapacity = 50;
             consumption = 10;
         }
-        if ("C".equals(this.category)){
+        if ("грузовая".equals(this.category)) {
             tankCapacity = 300;
             consumption = 20;
         }
     }
 
+    void determinationOfFuel() {
+        reserve = distance * consumption;
+        reserve = reserve / 100;
+        refueling = (int) Math.round ((reserve - tankCapacity * (tankFullness / 100)) / tankCapacity);
     }
+
+}
 
