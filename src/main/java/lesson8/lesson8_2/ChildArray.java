01989abb2175dbf01dbf2353eeabf162608ef1c9
Blonde-O <blonde-o@yandex.ru>
2021-11-30 17:03:17 +0300

    Проба с матрицами

---
+++ src/main/java/lesson8/lesson8_2/ChildArray.java
@@ -0,0 +1,31 @@
+package lesson8.lesson8_2;
+
+public class ChildArray extends GeneralArray{
+    public ChildArray(int n, int m) {
+        super(n, m);
+    }
+
+    public ChildArray() {
+    }
+
+    @Override
+    public void printSize() {
+        super.printSize();
+    }
+
+    @Override
+    public int[][] fillMatrix() {
+        return super.fillMatrix();
+    }
+
+    @Override
+    public int[][] multiplyMatrix() {
+        return super.multiplyMatrix();
+    }
+
+    @Override
+    public int[][] sqrtMatrix() {
+        return super.sqrtMatrix();
+    }
+}
+
---
+++ src/main/java/lesson8/lesson8_2/MatrixRunner.java
@@ -0,0 +1,19 @@
+package lesson8.lesson8_2;
+
+public class MatrixRunner {
+    public static void main(String[] args) {
+        GeneralArray matrix1 = new GeneralArray();
+        matrix1.printSize();
+        matrix1.fillMatrix();
+        matrix1.multiplyMatrix();
+        matrix1.sqrtMatrix();
+        matrix1.unitMatrix();
+
+
+
+
+
+    }
+
+
+}
