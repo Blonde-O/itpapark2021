ff845e1aef4e8efb937ee1cfd176a3d2cdb84432
Blonde-O <blonde-o@yandex.ru>
2021-12-03 13:35:06 +0300

    Задание №10

--- src/main/java/lesson10/Pressure.java
+++ src/main/java/lesson10/Pressure.java
@@ -2,26 +2,25 @@ package lesson10;
 
 public enum Pressure {
     BAR {
-
         public void action(double x) {
-            System.out.printf( "%.2f " + ATM.name()+ "; ", x *0.98692);
-            System.out.printf( "%.2f " + PSI.name()+ "\n", x *14.504);
+            System.out.printf("%.2f " + ATM.name() + "; ", x * 0.98692);
+            System.out.printf("%.2f " + PSI.name() + "\n", x * 14.504);
         }
     },
     ATM {
         public void action(double x) {
-            System.out.printf("%.2f " + BAR.name()+ "; ", x *1.01325);
-            System.out.printf( "%.2f " + PSI.name()+ "\n", x *14.696);
+            System.out.printf("%.2f " + BAR.name() + "; ", x * 1.01325);
+            System.out.printf("%.2f " + PSI.name() + "\n", x * 14.696);
         }
     },
     PSI {
         public void action(double x) {
-            System.out.printf( "%.2f " + ATM.name()+ "; ", x *0.068046);
-            System.out.printf("%.2f " + BAR.name()+ "\n", x *0.068948);
+            System.out.printf("%.2f " + ATM.name() + "; ", x * 0.068046);
+            System.out.printf("%.2f " + BAR.name() + "\n", x * 0.068948);
         }
     };
 
-    public abstract void action (double x);
+    public abstract void action(double x);
 }
 
 
--- src/main/java/lesson10/Runner.java
+++ src/main/java/lesson10/Runner.java
@@ -8,18 +8,18 @@ public class Runner {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите значение давления для конвертации");
         double x = in.nextDouble();
-        Pressure f1= Pressure.BAR;
-        Pressure f2= Pressure.ATM;
-        Pressure f3= Pressure.PSI;
+        Pressure f1 = Pressure.BAR;
+        Pressure f2 = Pressure.ATM;
+        Pressure f3 = Pressure.PSI;
         /*System.out.print(x+ " " +f1.name()+ "  равно: ");
         f1.action(x);
         System.out.print(x+ " " +f2.name()+ "  равно: ");
         f2.action(x);
         System.out.print(x+ " " +f3.name()+ "  равно: ");
         f3.action(x);*/
-        Pressure [] pressures = new Pressure[]{f1, f2, f3};
-        for (Pressure steps: pressures){
-            System.out.print(x+ " " +steps.name()+ "  равно: ");
+        Pressure[] pressures = new Pressure[]{f1, f2, f3};
+        for (Pressure steps : pressures) {
+            System.out.print(x + " " + steps.name() + "  равно: ");
             steps.action(x);
         }
     }
