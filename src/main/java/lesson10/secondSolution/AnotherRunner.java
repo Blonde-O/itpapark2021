024edce363c61d320ded46e1ee947678cdbfd812
Blonde-O <blonde-o@yandex.ru>
2021-12-03 22:27:31 +0300

    Конвертор величин давления

--- src/main/java/lesson10/secondSolution/AnotherRunner.java
+++ src/main/java/lesson10/secondSolution/AnotherRunner.java
@@ -1,15 +1,18 @@
 package lesson10.secondSolution;
+import java.util.Arrays;
 import java.util.Scanner;
 public class AnotherRunner {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите значение давления для конвертации");
         double x = in.nextDouble();
-        System.out.println("Введите единицу измерения, которую хотите преобразовать");
-        String first= in.next();
-        System.out.println("Введите единицу измерения,  в которую хотите преобразовать");
-        String second= in.next();
-        Convert convertion = new Convert(first,second, x);
+        System.out.println("Введите единицу измерения, которую хотите преобразовать "
+                + Arrays.toString(PressureSimple.values()));
+        String first = in.next();
+        System.out.println("Введите единицу измерения,  в которую хотите преобразовать "
+                + Arrays.toString(PressureSimple.values()));
+        String second = in.next();
+        Convert convertion = new Convert(first, second, x);
         convertion.startConverting();
     }
 
