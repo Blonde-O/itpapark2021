7b0d1346fbbd6dd335037d859d0ca9d9eac7e226
Blonde-O <blonde-o@yandex.ru>
2021-12-03 13:32:31 +0300

    Задание №10

--- src/main/java/lesson10/Runner.java
+++ src/main/java/lesson10/Runner.java
@@ -8,11 +8,20 @@ public class Runner {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите значение давления для конвертации");
         double x = in.nextDouble();
-        Pressure forward= Pressure.BAR;
-        System.out.print(x+ " " +forward.name()+ "  равно: ");
-        forward.action(x);
-        Pressure reverse= Pressure.ATM;
-        System.out.print(x+ " " +reverse.name()+ "  равно: ");
-        reverse.action(x);
+        Pressure f1= Pressure.BAR;
+        Pressure f2= Pressure.ATM;
+        Pressure f3= Pressure.PSI;
+        /*System.out.print(x+ " " +f1.name()+ "  равно: ");
+        f1.action(x);
+        System.out.print(x+ " " +f2.name()+ "  равно: ");
+        f2.action(x);
+        System.out.print(x+ " " +f3.name()+ "  равно: ");
+        f3.action(x);*/
+        Pressure [] pressures = new Pressure[]{f1, f2, f3};
+        for (Pressure steps: pressures){
+            System.out.print(x+ " " +steps.name()+ "  равно: ");
+            steps.action(x);
+        }
     }
+
 }
