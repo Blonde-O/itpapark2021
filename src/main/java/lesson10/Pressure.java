b754b87bb49283b9722d5d7e31d38909dfbbeead
Blonde-O <blonde-o@yandex.ru>
2021-12-03 13:15:55 +0300

    Задание №10

--- src/main/java/lesson10/Pressure.java
+++ src/main/java/lesson10/Pressure.java
@@ -2,13 +2,14 @@ package lesson10;
 
 public enum Pressure {
     BAR {
+
         public void action(double x) {
-            System.out.printf( "%.2f атм \n", x *0.98692);
+            System.out.printf( "%.2f " + ATM.name()+ "\n", x *0.98692);
         }
     },
     ATM {
         public void action(double x) {
-            System.out.printf("%.2f бар \n", x *1.01325);
+            System.out.printf("%.2f " + BAR.name()+ "\n", x *1.01325);
         }
     };
 
