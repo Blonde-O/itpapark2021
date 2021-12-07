d400560559dd34d9942e52f1e113da21bb7d2ee7
Blonde-O <blonde-o@yandex.ru>
2021-12-07 22:27:34 +0300

    Задание 11

--- src/main/java/lesson11/Runner.java
+++ src/main/java/lesson11/Runner.java
@@ -3,24 +3,20 @@ package lesson11;
 public class Runner {
 
 
-    public static void main(String[] args) {
-        int n, s, w;
-        double h, wi, l;
-        Vehicles car1 = new Vehicles();
-        Vehicles car2 = new Car();
-        Vehicles car3 = new Car();
-        Vehicles car4 = new Car();
-        Vehicles car5 = new Car();
-        Vehicles[] vehicles = new Vehicles[]{car1, car2, car3, car4, car5};
-        for (Vehicles steps : vehicles) {
-            steps.setNumber((int) (1000 - Math.random() * 1000));
-            steps.setSpeed((int) (Math.random() * 200));
-            steps.setWeight((int) (Math.random() * 8000));
-            steps.setHeight(Math.ceil((1.5+Math.random() * 3)*Math.pow(10,2))/Math.pow(10,2));
-            steps.setWidth(Math.random() * 1 + 2);
-            steps.setLength(Math.random() * 12);
-            System.out.println(steps);
-        }
+    public static void main(String[] args) throws Exception {
+        VehiclesGenerator v1 = new VehiclesGenerator();
+        v1.generator();
 
+        for (Vehicles steps : v1.getVehicles()) {
+            try {
+                if (steps.getSpeed() > 80) {
+                    throw new Exception("Автомобиль с номером " + steps.getNumber() + "! Вы превысили скорость 80 км/ч");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+
+        }
     }
 }
---
+++ src/main/java/lesson11/VehiclesGenerator.java
@@ -0,0 +1,51 @@
+package lesson11;
+
+public class VehiclesGenerator {
+    Vehicles car1 = new Car();
+    Vehicles car2 = new Car();
+    Vehicles car3 = new Car();
+    Vehicles car4 = new Car();
+
+
+    public Vehicles getCar1() {
+        System.out.println(car1);
+        return car1;
+    }
+
+    public Vehicles getCar2() {
+        return car2;
+    }
+
+    public Vehicles getCar3() {
+        return car3;
+    }
+
+    public Vehicles getCar4() {
+        return car4;
+    }
+
+    public Vehicles getCar5() {
+        return car5;
+    }
+
+    public Vehicles[] getVehicles() {
+        return vehicles;
+    }
+
+    Vehicles car5 = new Car();
+    Vehicles[] vehicles = new Vehicles[]{car1, car2, car3, car4, car5};
+
+        public void generator() {
+
+            for (Vehicles steps : vehicles) {
+                steps.setNumber((int) (1000 - Math.random() * 1000));
+                steps.setSpeed((int) (Math.random() * 200));
+                steps.setWeight((int) (Math.random() * 8000));
+                steps.setHeight(Math.ceil((1.5 + Math.random() * 3) * Math.pow(10, 2)) / Math.pow(10, 2));
+                steps.setWidth(Math.random() * 1 + 2);
+                steps.setLength(Math.random() * 12);
+                System.out.println(steps);
+            }
+        }
+
+}
