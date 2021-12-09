302428eff145f366773c1d297332565b44b8148c
Blonde-O <blonde-o@yandex.ru>
2021-12-09 14:55:03 +0300

    Задание №11

--- src/main/java/lesson11/Runner.java
+++ src/main/java/lesson11/Runner.java
@@ -4,53 +4,47 @@ public class Runner {
 
 
     public static void main(String[] args) throws Exception {
-        VehiclesGenerator v1 = new VehiclesGenerator();
-        v1.carGenerator();
-        v1.truckGenerator();
+        VehiclesGenerator generation = new VehiclesGenerator();
+        generation.carGenerator();
+        generation.truckGenerator();
 
-
-        for (Vehicles steps : v1.getVehicles()) {
+        for (Vehicles steps : generation.getVehicles()) {
             String message1 = "";
             String message2 = "";
             String message3 = "";
             String message4 = "";
             try {
                 if (steps.getSpeed() > 100) {
-                    throw new Exception(" превысил скорость 100 км/ч. !!!Объявляем розыск!!!");
+                    throw new Exception(" скорость 100 км/ч. !!!Объявляем розыск!!!");
                 }
-
             } catch (Exception a) {
                 message1 = a.getMessage();
-                //System.out.println( "Автомобиль с номером " + steps.getNumber() + " " +a.getMessage());
             }
             try {
                 if (steps.getSpeed() > 80 & steps.getSpeed() < 100) {
-                    throw new Exception(" превысил скорость 80 км/ч");
+                    throw new Exception(" скорость 80 км/ч");
                 }
             } catch (Exception b) {
                 message2 = b.getMessage();
-                //System.out.println( "Автомобиль с номером " + steps.getNumber() + " " +b.getMessage());
             }
             try {
                 if (steps.getWeight() > 8000) {
-                    throw new Exception(" превышает допустимую массу.");
+                    throw new Exception(" допустимую массу.");
                 }
-            } catch (Exception с) {
-                message3 = с.getMessage();
-                //System.out.println( "Автомобиль с номером " + steps.getNumber() + " " +с.getMessage());
+            } catch (Exception c) {
+                message3 = c.getMessage();
             }
             try {
                 if (steps.getHeight() > 4 && steps.getWidth() > 2.5) {
-                    throw new Exception(" превышает допустимую высоту и ширину.");
+                    throw new Exception(" допустимую высоту и ширину.");
                 }
             } catch (Exception d) {
                 message4 = d.getMessage();
-                //System.out.println( "Автомобиль с номером " + steps.getNumber() + " " +d.getMessage());
             }
             if (message1.equals("") & message2.equals("") & message3.equals("") & message4.equals("")) {
                 System.out.println(steps.getType() + " автомобиль с номером " + steps.getNumber() + " может проехать через КПП!");
             } else {
-                System.out.println(steps.getType() + " автомобиль с номером " + steps.getNumber() + "" + message1 + "" + message2 + "" + message3 + "" + message4);
+                System.out.println(steps.getType() + " автомобиль с номером " + steps.getNumber() + " превысил " + message1 + "" + message2 + "" + message3 + "" + message4);
             }
         }
     }
--- src/main/java/lesson11/VehiclesGenerator.java
+++ src/main/java/lesson11/VehiclesGenerator.java
@@ -1,44 +1,71 @@
 package lesson11;
 
 public class VehiclesGenerator {
-    Vehicles car1 = new Car();
-    Vehicles car2 = new Car();
-    /* Vehicles car3 = new Car();
-     Vehicles car4 = new Car();
-     Vehicles car5 = new Car();*/
-    Vehicles truck1 = new Truck();
-    Vehicles truck2 = new Truck();
-    Vehicles[] cars = new Vehicles[]{car1, car2};
-    Vehicles[] trucks = new Vehicles[]{truck1, truck2};
-    Vehicles[] vehicles = new Vehicles[]{car1, car2/*, car3, car4, car5*/, truck1, truck2};
+    Vehicles[] cars;
+    Vehicles[] trucks;
+    Vehicles[] vehicles;
+
+    public VehiclesGenerator() {
+        Vehicles car1 = new Car();
+        Vehicles car2 = new Car();
+        Vehicles car3 = new Car();
+        Vehicles car4 = new Car();
+        Vehicles car5 = new Car();
+        Vehicles car6 = new Car();
+        Vehicles car7 = new Car();
+        Vehicles car8 = new Car();
+        Vehicles car9 = new Car();
+        Vehicles car10 = new Car();
+        Vehicles car11 = new Car();
+        Vehicles car12 = new Car();
+        Vehicles car13 = new Car();
+        Vehicles car14 = new Car();
+        Vehicles car15 = new Car();
+        Vehicles truck1 = new Truck();
+        Vehicles truck2 = new Truck();
+        Vehicles truck3 = new Truck();
+        Vehicles truck4 = new Truck();
+        Vehicles truck5 = new Truck();
+        Vehicles truck6 = new Truck();
+        Vehicles truck7 = new Truck();
+        Vehicles truck8 = new Truck();
+        Vehicles truck9 = new Truck();
+        Vehicles truck10 = new Truck();
+        Vehicles truck11 = new Truck();
+        Vehicles truck12 = new Truck();
+        Vehicles truck13 = new Truck();
+        Vehicles truck14 = new Truck();
+        Vehicles truck15 = new Truck();
+        vehicles = new Vehicles[]{car1, car2, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12, car13, car14, car15, truck1, truck2, truck2, truck3, truck4, truck5, truck6, truck7, truck8, truck9, truck10, truck11, truck12, truck13, truck14, truck15};
+        trucks = new Vehicles[]{truck1, truck2, truck2, truck3, truck4, truck5, truck6, truck7, truck8, truck9, truck10, truck11, truck12, truck13, truck14, truck15};
+        cars = new Vehicles[]{car1, car2, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12, car13, car14, car15};
+    }
 
     public Vehicles[] getVehicles() {
         return vehicles;
     }
 
     public void carGenerator() {
-
         for (Vehicles steps : cars) {
             steps.setNumber((int) (99 + Math.random() * 900));
-            steps.setSpeed((int) (10+Math.random() * 150));
-            steps.setWeight((int) (1000+Math.random() * 2600));
+            steps.setSpeed((int) (10 + Math.random() * 150));
+            steps.setWeight((int) (1000 + Math.random() * 2600));
             steps.setHeight(Math.ceil((1.5 + Math.random() * 2) * Math.pow(10, 2)) / Math.pow(10, 2));
             steps.setWidth(Math.ceil((1.5 + Math.random() * 1.5) * Math.pow(10, 2)) / Math.pow(10, 2));
             steps.setLength(Math.ceil((3 + Math.random() * 3) * Math.pow(10, 2)) / Math.pow(10, 2));
-            System.out.println(steps);
+            //System.out.println(steps);
         }
     }
 
     public void truckGenerator() {
-
         for (Vehicles steps : trucks) {
             steps.setNumber((int) (99 + Math.random() * 900));
-            steps.setSpeed((int) (10+Math.random() * 91));
-            steps.setWeight((int) (3000+Math.random() * 8000));
+            steps.setSpeed((int) (10 + Math.random() * 91));
+            steps.setWeight((int) (3000 + Math.random() * 8000));
             steps.setHeight(Math.ceil((3 + Math.random() * 2) * Math.pow(10, 2)) / Math.pow(10, 2));
             steps.setWidth(Math.ceil((2.3 + Math.random()) * Math.pow(10, 2)) / Math.pow(10, 2));
             steps.setLength(Math.ceil((5 + Math.random() * 7) * Math.pow(10, 2)) / Math.pow(10, 2));
-            System.out.println(steps);
+            //System.out.println(steps);
         }
     }
 
