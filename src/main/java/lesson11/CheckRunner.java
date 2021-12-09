d4d58676402bfed15fc5935c8a2a480e5d2c627d
Blonde-O <blonde-o@yandex.ru>
2021-12-09 15:06:47 +0300

    Задание №11

---
+++ src/main/java/lesson11/CheckRunner.java
@@ -0,0 +1,10 @@
+package lesson11;
+
+public class CheckRunner {
+
+
+    public static void main(String[] args) throws Exception {
+        VehiclesChecker check = new VehiclesChecker();
+        check.startChecking();
+    }
+}
