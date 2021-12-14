6bdeb72afbd862306bf68216c198781c8181f6b2
Blonde-O <blonde-o@yandex.ru>
2021-12-14 12:41:36 +0300

    Задание №12

--- src/main/java/lesson12/Runner.java
+++ src/main/java/lesson12/Runner.java
@@ -7,11 +7,10 @@ import lesson12.sources.exceptions.MyArraySizeException;
 public class Runner {
 
     public static void main(String[] args) {
-        String[][] initial = {{}, {"0", "1", "2", "3"}, {"h", "5", "6", "7"}, {"0", "1", "2", "3"}, {"4", "5", "6", "7"}};
+        String[][] initial = {{}, {"0", "1", "2", "3", "3"}, {"g", "5", "6", "7"}, {"0", "1", "2", "3"}, {"4", "5", "6", "7"}};
         ArrayIntroduction probe = new ArrayIntroduction(initial);
         try {
-            probe.printArray();
-            probe.summarizeArray();
+            probe.workWithArray();
         } catch (MyArraySizeException e) {
             System.out.println(e.getMessage());
         } catch (MyArrayDataException e) {
--- src/main/java/lesson12/sources/ArrayIntroduction.java
+++ src/main/java/lesson12/sources/ArrayIntroduction.java
@@ -10,43 +10,27 @@ public class ArrayIntroduction {
         this.initialArray = initialArray;
     }
 
-    private void justPrint() {
-        System.out.print("Распечатка вашего массива");
-        for (String[] strings : initialArray) {
-            System.out.println();
-            for (String string : strings) {
-                System.out.print(string + " ");
-            }
-        }
-    }
-
-    public void printArray() {
+    public void workWithArray() {
+        int a;
+        int result = 0;
         if (!(initialArray.length == 4)) {
             throw new MyArraySizeException("Неправильный размер массива. Некорректное кол-во строк");
         }
-        for (String[] value : initialArray) {
-            for (int j = 0; j < value.length; j++) {
-                if (!(value.length == 4)) {
-                    throw new MyArraySizeException("Неправильный размер массива. Некорретное кол-во столбцов");
-                }
-            }
-        }
-        justPrint();
-    }
-
-    public void summarizeArray() {
-        int a;
-        int result = 0;
         for (int i = 0; i < initialArray.length; i++) {
             for (int j = 0; j < initialArray[i].length; j++) {
-                try {
-                    a = Integer.parseInt(initialArray[i][j]);
-                    result += a;
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("\nНеверный формат данных в ячейке с индексом " + i + "_" + j);
+                if (!(initialArray[i].length == 4)) {
+                    throw new MyArraySizeException("Неправильный размер массива. Некорретное кол-во столбцов");
+                } else {
+                    try {
+                        a = Integer.parseInt(initialArray[i][j]);
+                        result += a;
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException("\nНеверный формат данных в ячейке с индексом " + i + "_" + j);
+                    }
                 }
             }
         }
         System.out.println("\nСумма элементов массива равна " + result);
     }
 }
+
