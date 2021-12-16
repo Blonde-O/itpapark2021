f9a98ce77870cbfb483beb29377508d962feac95
Blonde-O <blonde-o@yandex.ru>
2021-12-16 10:01:40 +0300

    Задание №13

--- src/main/java/lesson13/Actions.java
+++ src/main/java/lesson13/Actions.java
@@ -1,20 +1,43 @@
 package lesson13;
 
-import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Actions {
 
     public void findWorkAge(int m, List list) {
+        int a = 0;
         System.out.println("Список ФИО работников, которые имеют стаж работы " + m + " лет.");
-        Iterator<Employee> employeesIterator = list.listIterator();
-        for (list.listIterator(); employeesIterator.hasNext(); ) {
-            Employee value = employeesIterator.next();
+        ListIterator<Employee> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            Employee value = iterator.next();
             if (value.getWorkAge() == m) {
                 System.out.println(value.getFio());
+                a++;
             }
         }
+        if (a == 0) {
+            System.out.println("Сотрудников с таким стажем не обнаружено");
+        }
+        System.out.println("-------------------------");
     }
 
+    public void deleteOddElements(List list) {
+        System.out.println("Произвели удаление нечетных сотрудников. Новый список имеет вид:");
+        ListIterator<Employee> interator = list.listIterator(list.size());
+        while (interator.hasPrevious()) {
+            if ((list.indexOf(interator.previous()) % 2) != 0) {
+                interator.remove();
+            }
+        }
+        while (interator.hasNext()) {
+            Employee value = interator.next();
+            System.out.println(value);
+        }
+    }
 }
+
+
+
+
 
--- src/main/java/lesson13/CollectionRunner.java
+++ src/main/java/lesson13/CollectionRunner.java
@@ -1,56 +1,52 @@
 package lesson13;
 
-import java.util.*;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CollectionRunner {
 
     public static void main(String[] args) {
-        int d = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите интресующий Вас стаж для поиска работников");
 
         List<Employee> listOfEmployees = new LinkedList<>();
-        listOfEmployees.add(new Employee("ДРА", 100, 5));
-        listOfEmployees.add(new Employee("ДЮК", 103, 12));
-        listOfEmployees.add(new Employee("КУБ", 213, 1));
-        listOfEmployees.add(new Employee("ФУХ", 516, 3));
-        listOfEmployees.add(new Employee("ЖУК", 608, 5));
-        listOfEmployees.add(new Employee("ЖУЕ", 102, 6));
-        listOfEmployees.add(new Employee("ЖАК", 951, 10));
-        listOfEmployees.add(new Employee("ЗНЕ", 345, 10));
-        listOfEmployees.add(new Employee("ХАУ", 412, 9));
-        listOfEmployees.add(new Employee("ТОР", 691, 5));
+        listOfEmployees.add(new Employee("ДРА", 101, 5));
+        listOfEmployees.add(new Employee("ДЮК", 102, 12));
+        listOfEmployees.add(new Employee("КУБ", 103, 1));
+        listOfEmployees.add(new Employee("ФУХ", 104, 3));
+        listOfEmployees.add(new Employee("ЖУК", 105, 5));
+        listOfEmployees.add(new Employee("ЖУЕ", 106, 6));
+        listOfEmployees.add(new Employee("ЖАК", 107, 10));
+        listOfEmployees.add(new Employee("ЗНЕ", 108, 10));
+        listOfEmployees.add(new Employee("ХАУ", 109, 9));
+        listOfEmployees.add(new Employee("ТОР", 110, 5));
+        listOfEmployees.add(new Employee("ДРА", 111, 30));
+        listOfEmployees.add(new Employee("ЦУМ", 112, 12));
+        listOfEmployees.add(new Employee("ЖИВ", 113, 1));
+        listOfEmployees.add(new Employee("БОК", 114, 19));
+        listOfEmployees.add(new Employee("ВАХ", 115, 0));
+        listOfEmployees.add(new Employee("ТОП", 116, 23));
+        listOfEmployees.add(new Employee("ПОТ", 117, 10));
+        listOfEmployees.add(new Employee("КАК", 118, 10));
+        listOfEmployees.add(new Employee("ВОР", 119, 8));
+        listOfEmployees.add(new Employee("БИМ", 120, 5));
+        listOfEmployees.add(new Employee("ЗУБ", 121, 4));
 
         Actions firstTry = new Actions();
-        //firstTry.findWorkAge(d, listOfEmployees);
 
-        /*int exp = 5;
-        for (Employee v : listOfEmployees) {
-            if (v.getWorkAge() == exp)
-                System.out.println(v.getFio());
+        try {
+            int workAge = in.nextInt();
+            if (workAge < 0) {
+                throw new IllegalArgumentException("Неккоретный ввод. Введено отричательное число.");
+            }
+            firstTry.findWorkAge(workAge, listOfEmployees);
+            firstTry.deleteOddElements(listOfEmployees);
+        } catch (InputMismatchException e) {
+            System.out.println("Неккоретный ввод. Число не введено.");
+        } catch (IllegalArgumentException f) {
+            System.out.println(f.getMessage());
         }
-        System.out.println("--------------------------");
-        Iterator<Employee> employeesIterator = listOfEmployees.listIterator();
-        for (listOfEmployees.listIterator(); employeesIterator.hasNext();){
-        Employee value = employeesIterator.next();
-            if (value.getWorkAge() == exp)
-                System.out.println(value.getFio());
-        }
- public void findWorkAge (List<Object> , int m){
-
-        }*/
-
-        //ListIterator<Employee> employeesIterator = listOfEmployees.listIterator();
-        for (ListIterator<Employee> employeesIterator = listOfEmployees.listIterator(); employeesIterator.hasPrevious();){
-
-            Employee value = employeesIterator.previous();
-            System.out.println(value);
-            /*if ((a % 2) != 0){
-               // System.out.println("удаляемый элемент" +value);
-                employeesIterator.remove();
-
-            }*/
-
-    }
-      // System.out.println(listOfEmployees);
-
     }
 }
