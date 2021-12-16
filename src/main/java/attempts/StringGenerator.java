fcc2745287fc096b9790df09410739a19d8ad52d
Blonde-O <blonde-o@yandex.ru>
2021-12-16 12:49:22 +0300

    Генератор случайной строки

---
+++ src/main/java/attempts/StringGenerator.java
@@ -0,0 +1,22 @@
+package attempts;
+
+import java.util.Random;
+
+public class StringGenerator {
+
+    public static void main(String[] args) {
+
+            int leftLimit = 1040; // letter 'a'
+            int rightLimit = 1103; // letter 'z'
+            int targetStringLength = 3;
+            Random random = new Random();
+
+            String generatedString = random.ints(leftLimit, rightLimit + 1)
+                    .limit(targetStringLength)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+
+            System.out.println(generatedString);
+
+    }
+}
