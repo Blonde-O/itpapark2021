7248da0d2650d599b3e69e1a1dbfb3d5e416363d
Blonde-O <blonde-o@yandex.ru>
2021-12-22 12:23:10 +0300

    Генератор случайной строки

--- src/main/java/lesson14/Runner.java
+++ src/main/java/lesson14/Runner.java
@@ -4,88 +4,53 @@ import java.util.*;
 
 public class Runner {
     public static void main(String[] args) {
-        int leftLimit = 1040; // letter 'a'
-        int rightLimit = 1103; // letter 'z'
-        int targetStringLength = 3;
-        String generatedString;
-        long number;
         long startTime = System.currentTimeMillis();
+        long f;
+        long b = 1;
         Random random = new Random();
+
         List<ProfileOfSubscriber> subscriberList = new ArrayList<>();
-        List <Long> boundedContacts = new ArrayList<>();
+        List<Long> boundedContacts = new ArrayList<>();
+        Set<Long> frequencyOfNumbers = new HashSet<>(boundedContacts);
+
         for (int i = 0; i < 300; i++) {
             subscriberList.add(ProfileOfSubscriber.generateSubscriber());
-
         }
-        //System.out.println(subscriberList);
 
-        for (ProfileOfSubscriber subscriber: subscriberList){
+
+        for (ProfileOfSubscriber subscriber : subscriberList) {
             int a = random.nextInt(200);
-            for (int i=0; i<a; i++){
-                ProfileOfSubscriber randomSubscriber= subscriberList.get(random.nextInt(subscriberList.size()));
-
+            for (int i = 0; i < a; i++) {
+                ProfileOfSubscriber randomSubscriber = subscriberList.get(random.nextInt(subscriberList.size()));
                 subscriber.setLinkedSubscribers(randomSubscriber.getPhone());
-
-
             }
-            //System.out.println(subscriber.getLinkedSubscribers());
             boundedContacts.addAll(subscriber.getLinkedSubscribers());
         }
-/*        System.out.println("_____________");
-        System.out.println(boundedContacts);
-        System.out.println("_____________");
-  */      // System.out.println(subscriberList);
-        Set<Long> unique = new TreeSet<>(boundedContacts);
-        long b=1;
-        for(Long x: unique){
-            if(Collections.frequency(boundedContacts, x)>=b){
-                b=Collections.frequency(boundedContacts, x);}
-            // System.out.println("Число " + x + ": " +Collections.frequency(boundedContacts, x));
 
-
-
-        }
-        //System.out.println(b);
-        long f=0;
-        for(Long x: unique){
-
-            if(Collections.frequency(boundedContacts, x)==b){
-                System.out.println("Наиболее популярный номер +7" + x + " повторяется " +Collections.frequency(boundedContacts, x)+ " раз");
-                f=x;
-                break;
-
-           /* for (ProfileOfSubscriber subscriber: subscriberList) {
-                if (subscriber.getPhone() == f) {
-                    System.out.println("Он принадлежит абоненту " + subscriber.getFio());
-                }
-            }*/
-
-
+        for (Long x : frequencyOfNumbers) {
+            if (Collections.frequency(boundedContacts, x) >= b) {
+                b = Collections.frequency(boundedContacts, x);
             }
         }
-        //System.out.println(f);
-         for (ProfileOfSubscriber subscriber: subscriberList){
-            if (subscriber.getPhone()==f){
+
+        for (Long x : frequencyOfNumbers) {
+            if (Collections.frequency(boundedContacts, x) == b) {
+                System.out.print("Наиболее популярный номер +7" + x + " повторяется " + Collections.frequency(boundedContacts, x) + " раз. ");
+                f = x;
+                for (ProfileOfSubscriber subscriber : subscriberList) {
+                    if (subscriber.getPhone() == f) {
+                        System.out.println("Он принадлежит абоненту " + subscriber.getFio());
+                    }
+                }
+            }
+        }
+        /*for (ProfileOfSubscriber subscriber : subscriberList) {
+            if (subscriber.getPhone() == f) {
                 System.out.println("Он принадлежит абоненту " + subscriber.getFio());
             }
-
-
-        }
-
-
-
-
-
-
-
-       /* private static ProfileOfSubscriber generateSubscriber () {
-            generatedString = random.ints(leftLimit, rightLimit + 1).limit(targetStringLength)
-                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                    .toString();
-            number = random.nextLong();
-            ProfileOfSubscriber subscriber = new ProfileOfSubscriber(generatedString, number);
         }*/
-        double time = (double)(System.currentTimeMillis() - startTime)/1000;
-        System.out.println("Время выполнения программы " +time+ " секунд.");
+
+        double time = (double) (System.currentTimeMillis() - startTime) / 1000;
+        System.out.println("Время выполнения программы " + time + " секунд.");
     }
 }
