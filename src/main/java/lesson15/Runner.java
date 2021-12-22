c89d4a861e39cb03754836d6ce42d0422835bc10
Blonde-O <blonde-o@yandex.ru>
2021-12-22 23:12:01 +0300

    Задание 15

--- src/main/java/lesson15/Runner.java
+++ src/main/java/lesson15/Runner.java
@@ -1,22 +1,36 @@
 package lesson15;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Runner {
 
     public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-    String [] splittedText =text.split(" ");
-        List<String> lines = new ArrayList<>();
-        text.lines().forEach(s -> lines.add(s));
-
-        System.out.println(lines);
-        for(String words:splittedText){
+        Scanner in = new Scanner(System.in);
+        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 1";
+        System.out.println(text);
+        String[] splittedText = text.split("\\p{Punct}");
+        for (String words : splittedText) {
             System.out.println(words);
         }
+        System.out.println("___________");
+        StringBuilder buildClearedWords = new StringBuilder();
+        for( int i =0; i< splittedText.length; i++){
+            buildClearedWords.append(splittedText[i]);
+        }
+        String newText = buildClearedWords.toString();
+        System.out.println(newText);
+        System.out.println("______________");
+        String[] clearText = newText.split(" ");
+        for (String words : clearText) {
+            System.out.println(words);
+        }
+        List<String> ListOfText = new ArrayList<>();
+        ListOfText = Arrays.asList(clearText);
+
 
     }
 }
+
