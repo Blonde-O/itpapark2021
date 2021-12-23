d348982634a25776fb27c0d94a5c4e64661f36b6
Blonde-O <blonde-o@yandex.ru>
2021-12-23 16:16:01 +0300

    Поиск популярных контактов

--- src/main/java/lesson15/Runner.java
+++ src/main/java/lesson15/Runner.java
@@ -1,35 +1,42 @@
 package lesson15;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Runner {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 1";
+        String text = "Lorem Sed ipsum dolor sit Sed? amet, consectetur  1 adipiscing elit. Sed 1";
         System.out.println(text);
+
+
+
         String[] splittedText = text.split("\\p{Punct}");
-        for (String words : splittedText) {
+       /*for (String words : splittedText) {
             System.out.println(words);
-        }
+        }*/
         System.out.println("___________");
         StringBuilder buildClearedWords = new StringBuilder();
-        for( int i =0; i< splittedText.length; i++){
+        for (int i = 0; i < splittedText.length; i++) {
             buildClearedWords.append(splittedText[i]);
         }
         String newText = buildClearedWords.toString();
-        System.out.println(newText);
+        /* System.out.println(newText);*/
         System.out.println("______________");
         String[] clearText = newText.split(" ");
-        for (String words : clearText) {
+       /*for (String words : clearText) {
             System.out.println(words);
-        }
-        List<String> ListOfText = new ArrayList<>();
-        ListOfText = Arrays.asList(clearText);
+        }*/
+        List<String> listOfText;
+        listOfText = Arrays.asList(clearText);
 
+        Stream<String> stream = listOfText.stream();
+        List<Object> resultedList = stream
+                .filter((x)-> Objects.equals(x, "1")).collect(Collectors.toList());
+        System.out.println(resultedList);
 
     }
 }
