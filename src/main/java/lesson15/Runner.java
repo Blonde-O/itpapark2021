587179f338f5a2fbd0ccd643824351733e01b641
Blonde-O <blonde-o@yandex.ru>
2021-12-24 12:13:06 +0300

    Задание 15

--- src/main/java/lesson15/Runner.java
+++ src/main/java/lesson15/Runner.java
@@ -11,27 +11,9 @@ public class Runner {
                 "tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula " +
                 "mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio " +
                 "nec mi tempor dignissim.";
-        // System.out.println(text);
-
-
-        String[] splittedText = text.split("\\p{Punct}");
-       /*for (String words : splittedText) {
-            System.out.println(words);
-        }*/
-        System.out.println("___________");
-        StringBuilder buildClearedWords = new StringBuilder();
-        for (int i = 0; i < splittedText.length; i++) {
-            buildClearedWords.append(splittedText[i]);
-        }
-        String newText = buildClearedWords.toString();
-        /* System.out.println(newText);*/
-        System.out.println("______________");
-        String[] clearText = newText.split(" ");
-       /*for (String words : clearText) {
-            System.out.println(words);
-        }*/
+        String[] splittedText = text.split("\\s*(\\W|\\s)\\s*");
         HashMap<String, Integer> finalOrder = new HashMap<>();
-        for (String i : clearText) {
+        for (String i : splittedText) {
             Integer value = finalOrder.get(i);
             if (value == null) {
                 finalOrder.put(i, 1);
@@ -39,16 +21,10 @@ public class Runner {
                 finalOrder.put(i, ++value);
             }
         }
-
         finalOrder.entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                 .map(Map.Entry::getKey)
                 .forEach(System.out::println);
-
-
-
     }
-
-
 }
 
