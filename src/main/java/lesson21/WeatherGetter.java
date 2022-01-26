b7fb955e839c668126bba0744c07f2917dc7ba34
Blonde-O <blonde-o@yandex.ru>
2022-01-26 16:49:45 +0300

    Дз 21

--- src/main/java/lesson21/WeatherGetter.java
+++ src/main/java/lesson21/WeatherGetter.java
@@ -11,20 +11,48 @@ import java.net.URLConnection;
 import java.util.Scanner;
 
 public class WeatherGetter {
+    final static String DEGREE  = "\u00b0";
+
     @SneakyThrows
     public static void main(String[] args) throws URISyntaxException, IOException {
+        String FORMAT_TEXT = "%s %s";
         String str;
-        String city = "Sochi";
+        String city = "Нижний+Новгород";
         String jsonFile = "src/main/resources/weather.json";
         String jsonPath = "$.main.temp";
 
-        URI uri = new URI("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&appid=187732d6e9dc1954003b42a70805b7b3&units=metric&");
-        URL url = uri.toURL();
-        URLConnection urlConnection = url.openConnection();
-        urlConnection.connect();
-        Scanner scanner = new Scanner(urlConnection.getInputStream());
-//        Scanner scanner = new Scanner(url.openStream())
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(jsonFile));
+        String [] cities = new String []{"Сочи", "Москва", "Волгоград", "Киев", "Архангельск", "Тюмень", "Саранск", "Воркута", "Норильск"};
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/weather.txt", true));
+        for (String town: cities) {
+            URI uri = new URI("https://api.openweathermap.org/data/2.5/weather?q=" + town + "&lang=ru&appid=187732d6e9dc1954003b42a70805b7b3&units=metric&");
+            URL url = uri.toURL();
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.connect();
+            Scanner scanner = new Scanner(urlConnection.getInputStream());
+            StringBuilder stringBuilder1 = new StringBuilder();
+            int character;
+            while ((character = urlConnection.getInputStream().read()) != -1) {
+                stringBuilder1.append((char) character);
+            }
+            String jsonAsString = stringBuilder1.toString();
+            Object temp = JsonPath.read(jsonAsString, jsonPath);
+            System.out.println("Текущая температура в городе " + town + " составляет: " + temp + " " + DEGREE + "C");
+
+
+            //String line = String.format(FORMAT_TEXT,"Текущая температура в городе" + town + " составляет: " + temp + " "  + DEGREE + "C\t");
+            bufferedWriter.write(String.format(FORMAT_TEXT,"Текущая температура в городе " + town + " составляет: " + temp + " "  + DEGREE + "C", ";"));
+            bufferedWriter.write(" \n");
+
+        }
+        bufferedWriter.close();
+
+
+
+
+
+//        /*Scanner scanner = new Scanner(url.openStream())
+        /*BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(jsonFile));
         while (scanner.hasNextLine()) {
             str = scanner.nextLine();
             bufferedWriter.write(str);
@@ -41,7 +69,7 @@ public class WeatherGetter {
         }
         String jsonAsString = stringBuilder.toString();
         double temp = JsonPath.read(jsonAsString, jsonPath);
-        System.out.println(temp);
+        System.out.println(temp);*/
 
     }
 }
