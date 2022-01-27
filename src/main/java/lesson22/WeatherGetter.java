1bbfaf69beb2984187629bf7b9460571c66fd785
Blonde-O <blonde-o@yandex.ru>
2022-01-27 11:36:26 +0300

    Дз 21

--- src/main/java/lesson22/WeatherGetter.java
+++ src/main/java/lesson22/WeatherGetter.java
@@ -3,67 +3,72 @@ package lesson22;
 import com.jayway.jsonpath.JsonPath;
 import lombok.SneakyThrows;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class WeatherGetter {
-    final static String DEGREE  = "\u00b0";
 
     @SneakyThrows
     public static void main(String[] args) throws URISyntaxException, IOException {
-        String FORMAT_TEXT = "%s %s";
-        String str;
-        String city = "Нижний+Новгород";
-        String jsonFile = "src/main/resources/weather.json";
         String temperaturePath = "$.main.temp";
         String humidityPath = "$.main.humidity";
-        String descriptionPath = "$.weather[*].description";
+        String descriptionPath = "$.weather[*].main";
         String windPath = "$.wind.speed";
-        city.replace("\s", "+");
+        String appIdPath = "src/main/resources/appid.txt";
 
-        String [] cities = new String []{"Сочи", "Москва", "Волгоград", "Киев", "Архангельск", "Тюмень", "Саранск", "Воркута", "Норильск"};
-
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/weather.txt", true));
-        for (String town: cities) {
-            URI uri = new URI("https://api.openweathermap.org/data/2.5/weather?q=" + town + "&lang=ru&appid=187732d6e9dc1954003b42a70805b7b3&units=metric&");
-            URL url = uri.toURL();
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.connect();
-            StringBuilder stringBuilder1 = new StringBuilder();
-            int character;
-            while ((character = urlConnection.getInputStream().read()) != -1) {
-                stringBuilder1.append((char) character);
-            }
-            String jsonAsString = stringBuilder1.toString();
-            Object temp = JsonPath.read(jsonAsString, temperaturePath);
-            Object description = JsonPath.read(jsonAsString, descriptionPath);
-
-            Object humidity = JsonPath.read(jsonAsString, humidityPath);
-            Object wind = JsonPath.read(jsonAsString, windPath);
-            System.out.println("Текущая погода в городе " + town + " :");
-            System.out.println("общая характеристика: " + description);
-            System.out.println("температура воздуха:  " + temp + " " + DEGREE + "C");
-            System.out.println("влажность воздуха: " + humidity);
-            System.out.println("скорость ветра:  " +wind+ " км/ч");
+        String[] cities = new String[]{"Сочи", "Москва", "Волгоград", "Нижний Новгород", "Архангельск", "Тюмень", "Саранск", "Воркута", "Норильск"};
+        String appId = extractAppId(appIdPath);
 
 
-            //String line = String.format(FORMAT_TEXT,"Текущая температура в городе" + town + " составляет: " + temp + " "  + DEGREE + "C\t");
-            bufferedWriter.write(String.format(FORMAT_TEXT,"Текущая температура в городе " + town + " составляет: " + temp + " "  + DEGREE + "C", ";"));
-            bufferedWriter.write(" \n");
-
+        for (String city : cities) {
+            StringBuilder stringBuilder = new StringBuilder();
+            URLConnection urlConnection = getUrlConnection(city, appId);
+            String jsonAsString = getString(stringBuilder, urlConnection);
+            Object temp = getObject(jsonAsString, temperaturePath);
+            Object description = getObject(jsonAsString, descriptionPath);
+            Object humidity = getObject(jsonAsString, humidityPath);
+            Object wind = getObject(jsonAsString, windPath);
+            System.out.println("Текущая погода в городе " + city + " :");
+            System.out.println(" - общая характеристика: " + description);
+            System.out.println(" - температура воздуха:  " + temp + " \u00b0" + "C");
+            System.out.println(" - влажность воздуха: " + humidity + "%");
+            System.out.println(" - скорость ветра:  " + wind + " км/ч");
         }
-        bufferedWriter.close();
+    }
 
+    private static String extractAppId(String appIdPath) throws IOException {
+        Path path = Paths.get(appIdPath);
+        String id = Files.readString(path);
+        return id;
+    }
 
+    private static Object getObject(String jsonAsString, String parameterPath) {
+        Object obj = JsonPath.read(jsonAsString, parameterPath);
+        return obj;
+    }
 
+    private static String getString(StringBuilder sb, URLConnection connection) throws IOException {
+        int character;
+        while ((character = connection.getInputStream().read()) != -1) {
+            sb.append((char) character);
+        }
+        String jsonAsString = sb.toString();
+        return jsonAsString;
+    }
 
-
-//
-
+    private static URLConnection getUrlConnection(String city, String appId) throws URISyntaxException, IOException {
+        city = city.replace("\s", "+");
+        URI uri = new URI("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&appid=" + appId +
+                "&units=metric&");
+        URL url = uri.toURL();
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.connect();
+        return urlConnection;
     }
 }
