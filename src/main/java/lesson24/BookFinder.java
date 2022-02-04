c3c57ce21fe1c78b076670a1edcdee93b11553d1
Blonde-O <blonde-o@yandex.ru>
2022-02-04 23:42:42 +0300

    ДЗ 24

--- src/main/java/lesson24/BookFinder.java
+++ src/main/java/lesson24/BookFinder.java
@@ -1,11 +1,13 @@
 package lesson24;
 
 import lesson24.classes.FindBook;
+import lombok.SneakyThrows;
 
 import java.util.Scanner;
 
 //Приложение, выполняющее поиск книги в БД
 public class BookFinder {
+    @SneakyThrows
     public static void main(String[] args) {
         int a;
         Scanner scanner = new Scanner(System.in);
@@ -15,13 +17,8 @@ public class BookFinder {
             String name = scanner.nextLine();
             if (name.toCharArray().length >= 3) {
                 a = 0;
-                try {
                     FindBook findBook = new FindBook(name);
                     findBook.searchBook();
-                } catch (Exception e) {
-                    System.out.println("База данных пуста. Наполните БД данными.");
-                    e.printStackTrace();
-                }
             } else {
                 a = 1;
                 System.out.println("Слишком короткий ввод, введите больше символов");
