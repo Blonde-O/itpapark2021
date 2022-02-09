d82f04a2d517f6297ea00451f47d5ddb47a941fb
Blonde-O <blonde-o@yandex.ru>
2022-02-09 23:43:38 +0300

    ДЗ 24

--- src/main/java/lesson26/Runner.java
+++ src/main/java/lesson26/Runner.java
@@ -9,21 +9,23 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class Runner {
     @SneakyThrows
+
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        URI uri = new URI("https://www.cbr-xml-daily.ru/daily_utf8.xml");
-        URL url = uri.toURL();
-        URLConnection urlConnection = url.openConnection();
-        urlConnection.connect();
+        String uriAddress = "https://www.cbr-xml-daily.ru/daily_utf8.xml";
+        String valuteCode = "BYN";
+        URLConnection urlConnection = getUrlConnection(uriAddress);
+        System.out.println(extracted(urlConnection, valuteCode));
        /* int character;
         try (InputStreamReader sr = new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8)
         ) {
@@ -41,18 +43,31 @@ public class Runner {
                 .evaluate(xmlDocument, XPathConstants.NUMBER);
         System.out.println(value);
     }*/
+
+    }
+
+    private static double extracted(URLConnection urlConnection, String valute) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+        double avvg;
         try (InputStream inputStream = urlConnection.getInputStream()) {
 
             Document xmlDocument = getXML(inputStream);
             XPath xPath = XPathFactory.newInstance().newXPath();
-            String avg = (String) xPath.compile("/ValCurs/Valute[Name='Венгерских форинтов']/Value/text()")
+            String avg = (String) xPath.compile("/ValCurs/Valute[CharCode='"+valute+"']/Value/text()")
                     .evaluate(xmlDocument, XPathConstants.STRING);
             avg = avg.replace(",", ".");
-            double nominal = (Double) xPath.compile("/ValCurs/Valute[Name='Венгерских форинтов']/Nominal")
+            double nominal = (Double) xPath.compile("/ValCurs/Valute[CharCode='"+valute+"']/Nominal")
                     .evaluate(xmlDocument, XPathConstants.NUMBER);
-            double avvg = Double.parseDouble(avg)/nominal;
-            System.out.println(avvg);
+            avvg = Double.parseDouble(avg)/nominal;
         }
+        return avvg;
+    }
+
+    private static URLConnection getUrlConnection(String str) throws URISyntaxException, IOException {
+        URI uri = new URI(str);
+        URL url = uri.toURL();
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.connect();
+        return urlConnection;
     }
 
     private static Document getXML(InputStream inputStream) throws ParserConfigurationException, SAXException, IOException {
