918fb12ca13c8235dd5121d29309d509fc9899d5
Blonde-O <blonde-o@yandex.ru>
2022-02-15 14:32:58 +0300

    26

--- src/main/java/attempts/spring/two/Runner.java
+++ src/main/java/attempts/spring/two/Runner.java
@@ -2,13 +2,27 @@ package attempts.spring.two;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Runner {
     public static void main(String[] args) {
         final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("attempts.spring.two");
          Printable printer = context.getBean("justsecondname", SecondNamePrint.class);
          Printable printerr = context.getBean(NamePrint.class);
-        printer.print();
+        /*printer.print();
         printerr.print();
-        printer.reverse();
+        printer.reverse();*/
+        Printable [] printering = new Printable[] {printer, printerr};
+        /*for (Printable printers : printering){
+            printers.print();
+            printers.reverse();
+        }*/
+        Stream<Printable> stream = Arrays.stream(printering);
+        stream.peek(x -> {x.print();
+            x.reverse();}).collect(Collectors.toList());
+
     }
 }
