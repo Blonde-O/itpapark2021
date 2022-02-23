1a31a500c03507a58be3ed0cc80bfeeb3b6a08d5
Blonde-O <blonde-o@yandex.ru>
2022-02-23 14:37:01 +0300

    ДЗ 28

---
+++ src/main/java/attempts/springshellapp/ApplicationCommands.java
@@ -0,0 +1,40 @@
+package attempts.springshellapp;
+
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
+import org.springframework.shell.standard.ShellOption;
+
+import javax.validation.constraints.Size;
+import java.util.Random;
+
+@ShellComponent
+public class ApplicationCommands {
+
+    @ShellMethod(value = "add two numbers", key = {"sum", "s"})
+    public int sum (int a, int b){
+        return a+b;
+    }
+
+    @ShellMethod(value = "greet someone", key = {"greet", "g"})
+    public String greeting (String name, @ShellOption (defaultValue = "Hello") String greeting){
+       return greeting+" "+name;
+    }
+
+    @ShellMethod(value = "change password", key = {"change", "ch"})
+    public String changePassword (@Size(min =4, max = 6) String pswd){
+        return "Пароль успешно изменен";
+    }
+@ShellMethod (value = "check connection", key = {"check", "con"})
+@ShellMethodAvailability
+    public String checkConnection(){
+        return isAvailble() ? Availability.available().getReason()
+                : Availability.unavailable("Соединение не доступно").getReason();
+    }
+
+    private boolean isAvailble(){
+        return new Random().nextBoolean();
+    }
+
+}
---
+++ src/main/java/attempts/springshellapp/ShellRunner.java
@@ -0,0 +1,13 @@
+package attempts.springshellapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellRunner {
+
+     public static void main(String[] args) {
+        SpringApplication.run(ShellRunner.class, args);
+
+    }
+}
