d10fdeb9e25d69aa965ccc4a661fd842367f8877
Blonde-O <blonde-o@yandex.ru>
2022-02-25 08:17:32 +0300

    30

--- src/main/resources/db/migrations/data.sql
+++ src/main/resources/db/migrations/data.sql
@@ -1,5 +1,4 @@
 -- Заполняем таблицу городов
-insert into CITY(post_code, name_russ, name_eng, population) values (603000, 'Нижний Новгород', 'Nizhny Novgorod', 1244254);
 insert into CITY(post_code, name_russ, name_eng, population) values (607220, 'Арзамас', 'Arzamas', 103400);
 insert into CITY(post_code, name_russ, name_eng, population) values (606650, 'Семенов', 'Semenov', 23902);
 insert into CITY(post_code, name_russ, name_eng, population) values (607600, 'Богородск', 'Bogorodsk', 33244);
